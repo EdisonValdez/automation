dbe077c20011f601ff661ccc9824acbdb092ca40
EdisonValdez <eddynvg@hotmail.com>
2024-09-26 13:39:41 +0200

    Removed all files and folders to other dir

--- OLD-AUTOMATION/17092024-automation/serializers.py
+++
@@ -1,8 +0,0 @@
-# serializers.py
-from rest_framework import serializers
-from .models import Business
-
-class BusinessSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Business
-        fields = ['id', 'title', 'description', 'status', 'city', 'address', 'phone', 'url', 'latitude', 'longitude', 'created_at', 'updated_at']
