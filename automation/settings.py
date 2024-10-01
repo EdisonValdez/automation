047b8aa73cb34ff3ac89ca9c3380e8ad0e53c541
EdisonValdez <eddynvg@hotmail.com>
2024-10-01 18:07:00 +0200

    api_key on digitalocean

--- automation/settings.py
+++ automation/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+
 DEBUG = 'True'
 ALLOWED_HOSTS = ['*']
 #ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
