5ca5b41b4d0604afc19bf01d229cb006f8c400e8
EdisonValdez <eddynvg@hotmail.com>
2024-10-01 14:20:35 +0200

    DISABLE_COLLECTSTATIC = 1

--- automation/settings.py
+++ automation/settings.py
@@ -131,6 +131,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+DISABLE_COLLECTSTATIC = 1
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
