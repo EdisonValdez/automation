172688dc58efc568c0e335213e8b5d2f87689699
Edison Valdez <eddynvg@hotmail.com>
2024-10-02 11:39:17 +0200

    Update settings.py
    staticfiles instead of static

--- automation/settings.py
+++ automation/settings.py
@@ -117,9 +117,9 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files and media files setup
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'  # Keep this for local development
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Local static files root
 
 # AWS S3 Configuration for static and media files
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
@@ -130,11 +130,12 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 # Static files storage with S3
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'  # Use this for production
 
 # Media files storage with S3
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
 
 # S3 settings
 AWS_S3_OBJECT_PARAMETERS = {
